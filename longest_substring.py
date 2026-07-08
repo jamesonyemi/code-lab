cdefc6ea847fdd675e5bed6bb31f5771bf4440db
paajay <jamesonyemi@gmail.com>
2026-07-08 20:51:10 +0000

    Refactor fixed size sliding window documentation and improve formatting; add longest substring function

---
+++ longest_substring.py
@@ -0,0 +1,18 @@
+
+
+from collections import defaultdict
+
+def longest_substring_without_repeating_character(s: str) -> int:
+    left = 0 
+    longest = 0
+    counter: dict[str, int] = defaultdict(int)
+    for right in range(len(s)):
+     counter[s[right]] += 1
+     while counter[s[right]] > 1:
+         counter[s[left]] -= 1
+         left += 1
+         longest = max(longest, right - left + 1)
+  
+    return longest       
+
+ 
--- non_zero_digits_classic.py
+++ non_zero_digits_classic.py
@@ -1,7 +1,8 @@
 """
 You are given an integer n.
 
-Form a new integer x by concatenating all the non-zero digits of n in their original order. If there are no non-zero digits, x = 0.
+Form a new integer x by concatenating all the non-zero digits of n in their original order. 
+If there are no non-zero digits, x = 0.
 
 Let sum be the sum of digits in x.
 
